e34e5fb29912c0f5cdf7be187d4fcd51ab2c9233
sebasoba <76491101+sebasoba@users.noreply.github.com>
2021-10-23 19:53:08 -0700

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,15 @@
+import modules time, os, shutil
+path = ''
+days = 
+time.time()
+isExist = os.path.exists(path)
+os.walk(path)
+os.path.join()
+os.stat(path)
+
+ctime = os.stat(path).st_ctime
+
+if (result>):
+    
+    
+    print('Not found')
